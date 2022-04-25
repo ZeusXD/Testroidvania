f71b0e93127acbea7c8141d3d939ff3a02b61f26
zeus freile <zeusb.f.m@gmail.com>
2022-04-25 00:26:28 -0300

    add holding jump

--- player/PlayerBlue.gd
+++ player/PlayerBlue.gd
@@ -9,6 +9,12 @@ const GRAVITY = 24
 const JUMPSPEED = 500
 const FLOOR = Vector2(0, -1)
 
+#Jumping 
+export var fallMultiplier = 2 
+export var lowJumpMultiplier = 20 
+export var jumpVelocity = 450 #Jump height
+export (int) var gravity = 8
+
 
 var velocity = Vector2.ZERO
 onready var animPlayer = $AnimatedSprite
@@ -17,36 +23,53 @@ func _physics_process(delta):
 	var input_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var input_y = Input.get_action_strength("ui_up")
 	
-	
 	if input_x < 0:
 		animPlayer.flip_h = true
 	elif input_x > 0:
 		animPlayer.flip_h = false
 	
+	idle(input_x, input_y)
+	running(input_x, delta)
+	#jump(input_y)
+	jumping(input_y)
+	
+	velocity = move_and_slide(velocity,  Vector2.UP)
+	
+func idle(input_x, input_y):
 	if input_x == 0 and input_y == 0 and is_on_floor():
 		animPlayer.play("idle")
-
+		
+	if not is_on_floor():
+		animPlayer.play("jump")
+	
+func running(input_x, delta):
 	if input_x != 0:
 		velocity = velocity.move_toward(Vector2(input_x, 0) * MAX_SPEED, ACCELERATION * delta)
 		if is_on_floor():
 			animPlayer.play("running")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
-	if not is_on_floor():
-		animPlayer.play("jump")
+	
+func jump(input_y):
 
 	if input_y != 0 and is_on_floor():
 		velocity.y = 0
 		velocity.y = -JUMPSPEED
-	
+		
 	velocity.y += GRAVITY
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	
-	
-	
-	
-	
-	
+func jumping(input_y):
+	#Applying gravity to player
+	velocity.y += gravity 
+
+	#Jump Physics
+	if velocity.y > 0: #Player is falling
+		velocity += Vector2.UP * (-9.81) * (fallMultiplier) #Falling action is faster than jumping action | Like in mario
+
+	elif velocity.y < 0 && Input.is_action_just_released("ui_up"): #Player is jumping 
+		velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier) #Jump Height depends on how long you will hold key
+
+	if is_on_floor():
+		if input_y != 0: 
+			velocity.y = -jumpVelocity #Normal Jump action
+
