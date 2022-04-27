9be059a0bc1a8090d357679716565a65e9fc8af5
zeus freile <zeusb.f.m@gmail.com>
2022-04-26 23:56:21 -0300

    implement jump and fall to kuro player

--- player/PlayerKuro.gd
+++ player/PlayerKuro.gd
@@ -5,6 +5,12 @@ const ACCELERATION = 960
 const MAX_SPEED = 150
 const FRICTION = 900
 
+#Jumping 
+export var fallMultiplier = 2 
+export var lowJumpMultiplier = 20 
+export var jumpVelocity = 450 #Jump height
+export (int) var gravity = 8
+
 var velocity = Vector2.ZERO
 onready var animPlayer = $AnimatedSprite
 
@@ -19,17 +25,36 @@ func _physics_process(delta):
 	
 	idle(input_x, input_y)
 	running(input_x, delta)
+	jump_fall(input_y)
 	
 	velocity = move_and_slide(velocity,  Vector2.UP)
 	
 func idle(input_x, input_y):
-	if input_x == 0:
+	if input_x == 0 and input_y == 0 and is_on_floor():
 		animPlayer.play("idle")
-		
 	
 func running(input_x, delta):
 	if input_x != 0:
 		velocity = velocity.move_toward(Vector2(input_x, 0) * MAX_SPEED, ACCELERATION * delta)
-		animPlayer.play("running")
+		if is_on_floor():
+			animPlayer.play("running")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+func jump_fall(input_y):
+	#Applying gravity to player
+	velocity.y += gravity 
+
+	#Jump Physics
+	if velocity.y > 0: #Player is falling
+		velocity += Vector2.UP * (-9.81) * (fallMultiplier) #Falling action is faster than jumping action | Like in mario
+		if not is_on_floor():
+			animPlayer.play("fall")
+		
+	elif velocity.y < 0 && Input.is_action_just_released("ui_up"): #Player is jumping 
+		velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier) #Jump Height depends on how long you will hold key
+		
+	if is_on_floor():
+		if input_y != 0: 
+			velocity.y = -jumpVelocity #Normal Jump action
+			animPlayer.play("jump")
